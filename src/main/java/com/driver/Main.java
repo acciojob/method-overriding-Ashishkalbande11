4c4af71121552c9cf71f615685945cb000a6f17a
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-03-13 12:01:52 +0530

    All class moved in main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,19 @@
 package com.driver;
 
-// Task1 class A created
-class A{
-    //Task 2 create meth method
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-//Task 3
-class B extends A{
-//    Task 4 method overriding
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
 public class Main {
+    public static class A{
+        //Task 2 create meth method
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    //Task 3
+    public static class B extends A {
+        //    Task 4 method overriding
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String args[]){
 //Task 5 creat obj og class B
         B obj = new B();
