ba40e716f0380c8f3f19af0acaf03430ddb4862b
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-03-13 11:55:56 +0530

    Method Overriding Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
+// Task1 class A created
+class A{
+    //Task 2 create meth method
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+//Task 3
+class B extends A{
+//    Task 4 method overriding
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
 public class Main {
-  
+    public static void main(String args[]){
+//Task 5 creat obj og class B
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
 }
